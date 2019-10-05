3bfc9cf5bce78ca281adfe484a39978b7fed1c6a
Ralph SotheGarnier <ralph@soethe.fr>
2019-10-05 12:15:09 +0200

    GameCreation+gamemanager

---
+++ Assets/Scripts/PreloadScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadScript : MonoBehaviour
+{
+
+	private void Awake()
+	{
+
+		if (LoadingSceneIntegration.otherScene > 0)
+		{
+			SceneManager.LoadScene(LoadingSceneIntegration.otherScene);
+		}
+		else
+		{
+			SceneManager.LoadScene("Menu");
+		}
+	}
+}
