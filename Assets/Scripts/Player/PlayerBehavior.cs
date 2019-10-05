7ebd05c3a662f2825b51195b9c2bca95ae602543
Ralph SotheGarnier <ralph@soethe.fr>
2019-10-05 13:49:00 +0200

    WaterGrid+First Steps player

---
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	//Variables for movement
+	public float MaxSpeed = 5f;
+
+	//Variable for Animator
+	private Animator playerAnim;
+
+	//Gamanager access
+	private GameManager manager;
+
+	// Use this for initialization
+	void Start () {
+		playerAnim = GetComponent<Animator>();
+		manager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)){
+			playerAnim.SetBool("isSwimming", true);
+		}
+		else {
+			playerAnim.SetBool("isSwimming", false);
+		}
+
+	}
+}
